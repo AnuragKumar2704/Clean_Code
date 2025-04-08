4b6654fe7054c3a1f4425c810454bf5082fb8693
AnuragKumar2704 <anurag.kumar@olx.in>
2025-04-08 20:05:15 +0530

    do change problem one

--- src/main/java/org/example/basic/ProblemOne.java
+++ src/main/java/org/example/basic/ProblemOne.java
@@ -4,9 +4,9 @@ import java.time.LocalDate;
 
 public class FoodInspection{
 
-    private LocalDate expirationDate;
-    private Boolean approvedForConsumption;
-    private Integer inspectorId;
+    private final LocalDate expirationDate;
+    private final Boolean approvedForConsumption;
+    private final Integer inspectorId;
 
     public FoodInspection(LocalDate expirationDate, Boolean approvedForConsumption, Integer inspectorId) {
         this.expirationDate = expirationDate;
