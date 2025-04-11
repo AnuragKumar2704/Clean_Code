5b16922def5acb56a3eb76fea036b02a7c6800fd
AnuragKumar2704 <anurag.kumar@olx.in>
2025-04-11 19:28:17 +0530

    format the code by adjusting proper brackets

--- src/main/java/org/example/exercises/OlxApp.java
+++ src/main/java/org/example/exercises/OlxApp.java
@@ -4,96 +4,96 @@ import java.util.Arrays;
 
 public class OlxApp {
 
-	private static final String AGED_WINE = "Aged Wine";
-	private static final String CONCERT_PASSES = "ColdPlay concert passes";
-	private static final String LEGENDARY_ITEM = "PokemonGo";
-	private static final int MAX_QUALITY = 50;
-	private static final int MIN_QUALITY = 0;
-	
-	public Item[] items;
+    private static final String AGED_WINE = "Aged Wine";
+    private static final String CONCERT_PASSES = "ColdPlay concert passes";
+    private static final String LEGENDARY_ITEM = "PokemonGo";
+    private static final int MAX_QUALITY = 50;
+    private static final int MIN_QUALITY = 0;
 
-	public OlxApp(Item[] items) {
-		this.items = items;
-	}
+    public Item[] items;
 
-	public static void main(String[] args) {
+    public OlxApp(Item[] items) {
+        this.items = items;
+    }
 
-		Item[] items = new Item[] { 
-							new Item("Mangoes", 10, 20)
-					   };
+    public static void main(String[] args) {
 
-		OlxApp app = new OlxApp(items);
+        Item[] items = new Item[]{
+                new Item("Mangoes", 10, 20)
+        };
 
-		app.updateQuality();
+        OlxApp app = new OlxApp(items);
 
-		System.out.println(app);
+        app.updateQuality();
 
-	}
+        System.out.println(app);
 
-	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
-			if (items[i].name != "Aged Wine" && items[i].name != "ColdPlay concert passes") {
-				if (items[i].quality > 0) {
-					if (items[i].name != "PokemonGo") {
-						items[i].quality = items[i].quality - 1;
-					}
-				}
-			} else {
-				// This part handles the items for which quality can increase
-				// "ColdPlay concert passes" and "Aged Wine"
+    }
 
-				if (items[i].quality < 50) {
-					items[i].quality = items[i].quality + 1;
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].name != "Aged Wine" && items[i].name != "ColdPlay concert passes") {
+                if (items[i].quality > 0) {
+                    if (items[i].name != "PokemonGo") {
+                        items[i].quality = items[i].quality - 1;
+                    }
+                }
+            } else {
+                // This part handles the items for which quality can increase
+                // "ColdPlay concert passes" and "Aged Wine"
 
-					if (items[i].name == "ColdPlay concert passes") {
-						if (items[i].sellIn < 11) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
 
-						if (items[i].sellIn < 6) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
-					}
-				}
-			}
+                    if (items[i].name == "ColdPlay concert passes") {
+                        if (items[i].sellIn < 11) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
 
-			// Everything except for Sulfuras the sellIn Decreases
-			if (items[i].name != "PokemonGo") {
-				items[i].sellIn = items[i].sellIn - 1;
-			}
+                        if (items[i].sellIn < 6) {
+                            if (items[i].quality < 50) {
+                                items[i].quality = items[i].quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
 
-			if (items[i].sellIn < 0) {
-				if (items[i].name != "Aged Wine") {
+            // Everything except for Sulfuras the sellIn Decreases
+            if (items[i].name != "PokemonGo") {
+                items[i].sellIn = items[i].sellIn - 1;
+            }
 
-					if (items[i].name != "ColdPlay concert passes") {
-						if (items[i].quality > 0) {
-							if (items[i].name != "PokemonGo") {
-								items[i].quality = items[i].quality - 1;
-							}
-						}
-					} else {
-						// For Backstage passes with sellin less than zero
-						// quality is set to zero
-						items[i].quality = 0;
-					}
-				} else {
-					// For Aged Wine below 50 quality increases actually by 2
-					// In the previous line
-					if (items[i].quality < 50) {
-						items[i].quality = items[i].quality + 1;
-					}
-				}
-			}
-		}
-	}
+            if (items[i].sellIn < 0) {
+                if (items[i].name != "Aged Wine") {
 
-	@Override
-	public String toString() {
-		return Arrays.toString(items);
-	}
+                    if (items[i].name != "ColdPlay concert passes") {
+                        if (items[i].quality > 0) {
+                            if (items[i].name != "PokemonGo") {
+                                items[i].quality = items[i].quality - 1;
+                            }
+                        }
+                    } else {
+                        // For Backstage passes with sellin less than zero
+                        // quality is set to zero
+                        items[i].quality = 0;
+                    }
+                } else {
+                    // For Aged Wine below 50 quality increases actually by 2
+                    // In the previous line
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
 
 }
